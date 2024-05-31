75ae8e91dec8f08ee92859b890beb6a9bcaf3cbe
bye <bye@byecorps.com>
2024-05-31 20:43:48 +0100

    Clearing profile.php will remake soon

--- profile.php
+++ profile.php
@@ -19,26 +19,5 @@ if (empty($profile)) {
     ];
 }
 
-$avatar = "/assets/default.png";
-$display_name = "";
-
-if ($_SESSION['id'] != $profile['id']) {
-    $avatar = get_avatar_url($profile['id']);
-    if ($profile['public_display_name']) {
-        $display_name = get_display_name($profile['id'], false);
-    }
-} else {
-    $avatar = get_avatar_url($profile['id']);
-    $display_name = get_display_name($profile['id'], false);
-}
-
-// Get badges owned by this person
-$badges = db_execute_all('SELECT * FROM badge_owners INNER JOIN badges b on badge_owners.badge_id = b.id WHERE owner_id = ?; ', [$profile['id']]);
-if (!empty($badges)) {
-    if (!array_is_list($badges)) {
-        $badges = array (0 => $badges);
-    }
-}
-
 ?>
 
