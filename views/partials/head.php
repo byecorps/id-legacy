8ac041536a46bb3e5a77afa2b5da0e6ee6ab7469
bye <bye@byecorps.com>
2024-07-03 14:20:15 +0100

    Favicon

--- views/partials/head.php
+++ views/partials/head.php
@@ -1,12 +1,16 @@
+<?php
+if (isset($page_title)) {
+    echo '<title>' . $page_title . ' ~> ByeCorps ID</title>';
+}
+?>
 <meta charset="UTF-8">
 <meta name="viewport"
       content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <script src="https://cdn.byecorps.com/global/fonts/fontawesome/js/all.min.js" defer></script>
 <link rel="stylesheet" href="/styles/base.css">
+<link rel="shortcut icon" href="/favicon.svg" type="image/x-icon">
 
-<?php
-if (isset($page_title)) {
-    echo '<title>' . $page_title . '</title>';
-}
-?>
+<!-- Apple web app stuff -->
+<link rel="apple-touch-icon" href="https://cdn.id.byecorps.com/assets/icons/apple-icon-180.png" />
+<link rel="apple-touch-startup-image" href="" />
