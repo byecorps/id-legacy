c11542e512f506bdb8b87a3460d8ab762745f52f
Bye <bye@byecorps.com>
2024-07-05 10:53:02 +0100

    New execution time footer setup

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,4 +1,9 @@
 <footer>
-    <div>&copy; ByeCorps 2024 :: <a href="/credits"><?= get_string('page.credits') ?></a></div>
-    <div><b><?= get_string('footer.executionTime') ?>: </b> <?= round((microtime(true) - $_SERVER['REQUEST_TIME_FLOAT']) * 1000, 3) ?> ms</div>
+    <div class="item">
+        <div>&copy; ByeCorps 2024 :: <a href="/credits"><?= get_string('page.credits') ?></a></div>
+        <div><?= get_string('footer.executionTime', ['time'=>round((microtime(true) - $_SERVER['REQUEST_TIME_FLOAT']) * 1000, 3)]) ?></div>
+    </div>
+    <div class="item">
+        <script src="/scripts/langauge_switcher.js" defer></script>
+    </div>
 </footer>
